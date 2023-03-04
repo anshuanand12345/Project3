2dab8e258741ac92b4459a614962e7fabf95536f
Anshu Anand <anshuanand@Anshus-MacBook-Air.local>
2023-03-04 21:01:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [active, setActive] = useState(false);
+  const changeBg = () => {
+    setActive(!active);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='page'>
+      <button onClick={changeBg} style={{ backgroundColor: active ? "red" : "blue" }}> {active ? "True" : "False" } </button>
+   </div>
   );
 }
 
